28da629fafba65dbd5c391e3cf2d0c44a2b9c3a8
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2022-12-15 12:52:38 -0300

    Comment code

--- game/special_movements/en_passant.rb
+++ game/special_movements/en_passant.rb
@@ -7,18 +7,28 @@ module EnPassant
     def move(color, position, position2)
         return false unless super
 
-        if position2.to_a == @en_passant && position2.piece.is_a?(Pawn)
-            @en_passant_piece.piece = Empty.new
-            @en_passant = []
-            @en_passant_piece = nil
         # If a Pawn has moved two places vertically, add the position in between to the
         # variable en_passant. en_passant_piece will be the position to which the piece
         # has moved
-        elsif position2.piece.is_a?(Pawn) && (position2 - position)[0].abs == 2
-            @en_passant = [position.x, position.y + (color == :white ? 1 : -1)]
-            @en_passant_piece = position2
+        if position2.piece.is_a?(Pawn) && (position2 - position)[1].abs == 2
+            set [position.x, position.y + (color == :white ? 1 : -1)], position2
+        else
+            # If an en passant is being performed, kill the Pawn
+            if position2 == @en_passant && position2.piece.is_a?(Pawn)
+                @en_passant_piece.piece = Empty.new
+            end
+
+            # Reset variables
+            set [], nil
         end
 
         true
+    end
+
+    private
+
+    def set(en_passant, en_passant_piece)
+        @en_passant = en_passant
+        @en_passant_piece = en_passant_piece
     end
 end
